36127c2ab19e058a0d4a7108724752785041f848
tomchris <tom.christopoulos@platform9.com>
2020-01-22 15:28:25 -0500

    Updates #13 Updated setup.py to use CLI dev branch
    Until new full devel deployment is complete install can be via 2
    methods:
    User:
    - From virtualenv `pip install git+git://github.com/platform9/express-wizard.git@master#egg=express-wizard`
    Dev:
    - `git clone git://github.com/platform9/express-wizard.git`
    - `cd express-wizard && pip install -e .`

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 """Install express-wizard"""
 
 from setuptools import setup, find_packages
+import sys
 import os
-from os import system 
 from os import path
 from io import open
 
@@ -11,35 +11,10 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-#   def run_cmd(cmd):
-#       cmd_stdout = ""
-#       tmpfile = "/tmp/pf9.{}.tmp".format(os.getppid())
-#       cmd_exitcode = os.system("{} > {} 2>&1".format(cmd, tmpfile))
-
-#       # read output of command
-#       if os.path.isfile(tmpfile):
-#           try:
-#               fh_tmpfile = open(tmpfile, 'r')
-#               cmd_stdout = fh_tmpfile.readlines()
-#           except:
-#               None
-
-#       os.remove(tmpfile)
-#       return cmd_exitcode, cmd_stdout
-
-
-#   clone_express_cli = "git clone {}".format("git@github.com:platform9/express-cli.git")
-#   run_cmd(clone_express_cli)
-#   #system("{}".format(clone_express_cli)) 
-
-#   print("cwd: {}".format(os.system("pwd")))
-#   switch_branch = "cd {} && git checkout {}".format("express-cli", "tomchris/setuptools")
-#   #system("{}".format(switch_branch)) 
-#   run_cmd(switch_branch)
-
-#   install_cli = "cd {} && pip install -e .".format("express-cli")
-#   #system("{}".format(install_cli)) 
-#   run_cmd(install_cli)
+# TODO: Write IF statement to determine if install is dev (local) or prod (pulling repos until pipy)
+#       Set only one value below based on [remote or local] and [master or branch]
+#       master 'express-cli @ git+git://github.com/platform9/express-cli.git@master#egg=express-cli',
+express_cli_source = ('express-cli @ git+git://github.com/platform9/express-cli.git@tomchris/restructure#egg=express-cli')
 
 setup(
     # $ pip install express-wizard
@@ -64,11 +39,7 @@ setup(
 	'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=['docs', 'tests*']),
-    #packages=find_packages() + find_packages(where='./lib'),  # Required
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
-#    dependency_links=[
-#        ''.join(['file:\\', os.path.join(os.getcwd(), 'express-cli#egg=express-cli')])
-#        ],
     install_requires=[
         'requests',
         'urllib3',
@@ -76,7 +47,7 @@ setup(
         'argparse',
         'pprint',
         'openstacksdk==0.12.0',
-        'express-cli @ git+git://github.com/platform9/express-cli.git@tomchris/restructure#egg=express-cli',
+        'express-cli @ git+file://home/tomchris/Development/express-cli#egg=express-cli',
         ],
     entry_points = {
         'console_scripts': [
