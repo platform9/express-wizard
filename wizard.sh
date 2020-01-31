d8bf989621b72b6419293b079429d78fd00572b0
tomchris <tom.christopoulos@platform9.com>
2020-01-31 05:45:02 -0500

    Conveged setup bash script

--- wizard.sh
+++ wizard.sh
@@ -14,81 +14,101 @@
 #
 ####################################################################################################
 
+set -o pipefail
+
 start_time=$(date +"%s.%N")
 
 assert() {
-    if [ $# -gt 0 ]; then echo "ASSERT: $(basename $0) : ${1}"; fi
+    if [ $# -gt 0 ]; then stdout_log "ASSERT: ${1}"; fi
+    echo -e "\n\n"
+    echo "$(tail ${log_file})"
+    echo ""
+    echo "Installation failed, Here are the last 10 lines from the log" 
+    echo "The full installation log is available at ${log_file}"
+    echo "If more information is needed re-run the install with --debug"
     exit 1
 }
 
 debugging() {
+    if [[ ${debug_flag} ]]; then stdout_log "${1}"; fi
+}
+
+stdout_log(){
     if [[ ${debug_flag} ]]; then
-	echo "DEBUGGING: $(date +"%T") : $(bc <<<$(date +"%s.%N")-${start_time}) :$(basename $0) : ${1}"
+	if (which bc > /dev/null 2>&1); then
+	    output="DEBUGGING: $(date +"%T") : $(bc <<<$(date +"%s.%N")-${start_time}) :$(basename $0) : ${1}"
+	else
+	    output="DEBUGGING: $(date +"%T") : $(basename $0) : ${1}"
+	fi            
+        echo "${output}" 2>&1 | tee -a ${log_file}
+    else
+        echo "$1" 2>&1 | tee -a ${log_file}
     fi
 }
 
-# parse commandline
-for i in "$@"; do
-  case $i in
-    -h|--help)
-	echo "Usage: $(basename $0)"
-	echo "	  [-i|--init]"
-	echo "	  [-t|--test]"
-	echo "	  [-l|--local]"
-	echo "	  [-d|--debug=]"
-	echo "	  [-c|--config=]"
-	echo "	  [-b|--branch=]"
+parse_args() {
+    for i in "$@"; do
+      case $i in
+	-h|--help)
+	    echo "Usage: $(basename $0)"
+	    echo "	  [-i|--init]"
+	    echo "	  [-t|--test]"
+	    echo "	  [-l|--local]"
+	    echo "	  [-d|--debug=]"
+	    echo "	  [-c|--config=]"
+	    echo "	  [-b|--branch=]"
+	    echo ""
+	    exit 0
+	    shift
+	    ;;
+	-b=*|--branch=*)
+	    if [ -n ${i#*=} ]; then
+		wizard_branch="${i#*=}"
+	    else
+		assert "Branch name must be provided";
+	    fi
+	    shift
+	    ;;
+	-c=*|--config=*)
+	    config_file="${i#*=}"
+	    if [ -z ${config_file} ];
+		then assert "config file not provided: ${config_file}" 
+	    elif [[ ! -r ${config_file} ]];
+		then assert "failed to access config file: ${config_file}";
+	    fi
+	    shift
+	    ;;
+	-d|--debug)
+	    debug_flag="${i#*=}"
+	    shift
+	    ;;
+	-d=*|--debug=*)
+	    debug_flag="${i#*=}"
+	    shift
+	    ;;
+	-l|--local)
+	    run_local="--local"
+	    shift
+	    ;;
+	-t|--test)
+	    test_wizard="--test"
+	    shift
+	    ;;
+	-i|--init)
+	    init_flag="--init"
+	    shift
+	    ;;
+	*)
+	echo "$i is not a valid command line option."
 	echo ""
-	exit 0
-        shift
-        ;;
-    -b=*|--branch=*)
-	if [ -n ${i#*=} ]; then
-	    wizard_branch="${i#*=}"
-	else
-	    assert "Branch name must be provided";
-	fi
-	shift
+	echo "For help, please use $0 -h"
+	echo ""
+	exit 1
 	;;
-    -c=*|--config=*)
-	config_file="${i#*=}"
-	if [ -z ${config_file} ];
-	    then assert "config file not provided: ${config_file}" 
-	elif [[ ! -r ${config_file} ]];
-	    then assert "failed to access config file: ${config_file}";
-	fi
+	esac
 	shift
-	;;
-    -d|--debug)
-	debug_flag="${i#*=}"
-	shift
-        ;;
-    -d=*|--debug=*)
-	debug_flag="${i#*=}"
-	shift
-        ;;
-    -l|--local)
-        run_local="--local"
-	shift
-        ;;
-    -t|--test)
-        test_wizard="--test"
-	shift
-        ;;
-    -i|--init)
-        init_flag="--init"
-        shift
-        ;;
-    *)
-    echo "$i is not a valid command line option."
-    echo ""
-    echo "For help, please use $0 -h"
-    echo ""
-    exit 1
-    ;;
-    esac
-    shift
-done
+    done
+}
 
 init_venv_python() {
     if [[ ${python_version} == 2 ]]; then
@@ -96,77 +116,119 @@ init_venv_python() {
     else 
         pyver="3";
     fi
-    echo "Initializing Virtual Environment using Python ${python_version}"
+    stdout_log "Initializing Virtual Environment using Python ${python_version}"
     #Validate and initialize virtualenv
-    if [ "$(virtualenv --version -p python${pyver} > /dev/null 2>&1; echo $?)" -ne 0 ]; then
-        #Validating pip
-	which pip > /dev/null 2>&1
-	if [ $? -ne 0 ]; then
-            echo "ERROR: missing package: pip (attempting to install using get-pip.py)"
+    if ! (virtualenv --version > /dev/null 2>&1); then
+        debugging "Validating pip"
+	if ! which pip > /dev/null 2>&1; then
+            debugging "ERROR: missing package: pip (attempting to install using get-pip.py)"
             curl -s -o ${pip_path} ${pip_url}
             if [ ! -r ${pip_path} ]; then assert "failed to download get-pip.py (from ${pip_url})"; fi
-            python${pyver} ${pip_path}
-            if [ $? -ne 0 ]; then
-                echo "ERROR: failed to install package: pip (attempting to install via 'sudo get-pip.py')"
-                sudo python${pyver} ${pip_path} > /dev/null 2>&1
-                if [ $? -ne 0 ]; then
+            
+            if [ ! python${pyver} ${pip_path} ]; then
+                debugging "ERROR: failed to install package: pip (attempting to install via 'sudo get-pip.py')"
+                if [ sudo python${pyver} ${pip_path} > /dev/null 2>&1 ]; then
                     assert "Please install package: pip"
                 fi
             fi
         fi
-	echo "ERROR: missing python package: virtualenv (attempting to install via 'pip install virtualenv')"
+	debugging "ERROR: missing python package: virtualenv (attempting to install via 'pip install virtualenv')"
         # Attemping to Install virtualenv
-	pip${pyver} install virtualenv > /dev/null 2>&1
-        if [ $? -ne 0 ]; then
-            echo "ERROR: failed to install python package (attempting to install via 'sudo pip install virtualenv')"
-            sudo pip${pyver} install virtualenv > /dev/null 2>&1
-            if [ $? -ne 0 ]; then
+        if [ ! pip${pyver} install virtualenv > /dev/null 2>&1 ]; then
+            debugging "ERROR: failed to install python package (attempting to install via 'sudo pip install virtualenv')"
+            if ! sudo pip${pyver} install virtualenv > /dev/null 2>&1; then
                 assert "Please install the 'virtualenv' module using 'pip install virtualenv'"
             fi
         fi
     fi
-    cd ${wizard_basedir}
-    virtualenv -p python${pyver} ${wizard_venv} > /dev/null 2>&1
+    if ! (virtualenv -p python${pyver} ${venv} > /dev/null 2>&1); then assert "Creation of virtual environment failed"; fi
+    debugging "venv_python:"${venv_python}
     if [ ! -r ${venv_python} ]; then assert "failed to initialize virtual environment"; fi
-
     debugging "Upgrade pip"
-    # upgrade pip
-    (. ${venv_activate} && pip install pip --upgrade > /dev/null 2>&1)
+    if ! (${venv_python} -m pip install pip --upgrade > /dev/null 2>&1); then assert "Pip upgrade failed"; fi
 }
+
+initialize_basedir() {
+    debugging "Initializing: ${pf9_basedir}"
+    if [[ -n "${init_flag}" ]]; then
+	debugging "DELETEING pf9_basedir: ${pf9_basedir}"
+	if [ -d "${pf9_basedir}" ]; then
+	    rm -rf "${pf9_basedir}"
+	    if [ -d "${pf9_basedir}" ]; then assert "failed to remove ${pf9_basedir}"; fi
+	fi
+    fi
+    debugging "Ensuring ${pf9_basedir} Exist"
+    if ! mkdir -p "${pf9_basedir}" > /dev/null 2>&1; then assert "failed to create directory: ${pf9_basedir}"; fi
+    debugging "Ensuring $(dirname ${log_file}) Exist"
+    if ! mkdir -p "$(dirname ${log_file})" > /dev/null 2>&1; then assert "failed to create log directory: $(dirname ${log_file})"; fi
+    debugging "Ensuring ${log_file} Exist"
+    if ! touch "${log_file}" > /dev/null 2>&1; then assert "failed to create log file: ${log_file}"; fi
+    debugging "Ensuring $(dirname ${pf9_bin}) Exist"
+    if ! mkdir -p "${pf9_bin}" > /dev/null 2>&1; then assert "failed to create bin directory: $(dirname ${pf9_bin})"; fi
+    if [ ! -d "${pf9_basedir}" ]; then assert "failed to create directory: ${pf9_basedir}"; fi
+}
+
+
+
 
 ## main
 
+parse_args "$@"
+cd ~
+# Set global variables
 if [ -z ${wizard_branch} ]; then wizard_branch=master; fi
-wizard_basedir=~/.pf9-wizard
-venv_python=${wizard_venv}/bin/python
-venv_activate=${wizard_venv}/bin/activate
-wizard_url=https://raw.githubusercontent.com/platform9/express-wizard/${wizard_branch}/wizard.py
-pip_url=https://bootstrap.pypa.io/get-pip.py
-pip_path=/tmp/get_pip.py
-
+# Set the path so double quotes don't use the litteral '~'
+pf9_basedir=$(dirname ~/pf9/.) 
+pip_path=${pf9_basedir}/get_pip.py
+log_file=${pf9_basedir}/log/wizard_install.log
+pf9_bin=${pf9_basedir}/bin
+venv="${pf9_basedir}/pf9-venv"
+venv_python="${venv}/bin/python"
+venv_activate="${venv}/bin/activate"
+wizard_url="git+git://github.com/platform9/express-wizard.git@${wizard_branch}#egg=express-wizard"
+#https://raw.githubusercontent.com/platform9/express-wizard/${wizard_branch}/wizard.py"
+pip_url="https://bootstrap.pypa.io/get-pip.py"
+wizard_entrypoint=$(dirname ${venv_python})/wizard
+cli_entrypoint=$(dirname ${venv_python})/express
+wizard=${pf9_bin}/wizard
+cli=${pf9_bin}/cli
 
 # initialize installation directory
+initialize_basedir
 
-if [ -n ${run_local} ];then
-    wizard_basedir=$(pwd)
-
+# configure python virtual environment
+stdout_log "Configuring virtualenv"
+if [ ! -f "${venv_activate}" ]; then
+    debugging "Virtual Environment: ${venv} Doesn't not exist, Configuring."
+    for ver in {3,2,''}; do #ensure python3 is first
+	debugging "Checking Python${ver}: $(which python${ver})"
+        if (which python${ver} > /dev/null 2>&1); then
+	    python_version="$(python${ver} <<< 'import sys; print(sys.version_info[0])')"
+	    stdout_log "Python Version Selected: python${python_version}"
+	    break
+        fi
+    done
+    init_venv_python
 else
-    if [[ -n ${init_flag} ]]; then
-	debugging "DELETEING wizard_basedir: ${wizard_basedir}"
-	if [ -d ${wizard_basedir} ]; then
-	    rm -rf ${wizard_basedir}
-	    if [ -d ${wizard_basedir} ]; then assert "failed to remove ${wizard_basedir}"; fi
-	fi
-    fi
-    if [ ! -d ${wizard_basedir} ]; then
-	mkdir -p ${wizard_basedir}
-	if [ ! -d ${wizard_basedir} ]; then assert "failed to create directory: ${wizard_basedir}"; fi
-    fi
+    stdout_log "INFO: using exising virtual environment"
+    debugging "Upgrade pip"
+    if ! (${venv_python} -m pip install pip --upgrade > /dev/null 2>&1); then assert "Pip upgrade failed"; fi
 fi
 
-# validate python stack
-which python > /dev/null 2>&1
-if [ $? -ne 0 ]; then assert "Python stack missing"; fi
+stdout_log "Installing Platform9 Express Management Suite"
+if ! (${venv_python} -m pip install -e  ${wizard_url} > /dev/null 2>&1); then
+    assert "Installation of Platform9 Express Wizard Failed"; fi
+if ! (wizard --help > /dev/null 2>&1); then assert "Unable to launch Platform9 Wizard"; fi
+if [ ! -f ${wizard} ]; then
+    if ! (ln -s ${wizard_entrypoint} ${wizard} > /dev/null 2>&1); then
+	assert "failed to create Express-Wizard symlink: ${wizard_entrypoint} ${wizard}"; fi
+fi
+stdout_log "Installing Platform9 Express Management Environment"
+if ! (express init > /dev/null 2>&1); then assert "Initialization of Platform9 Express-CLI Failed"; fi
+if [ ! -f ${cli} ]; then
+    if ! (ln -s ${cli_entrypoint} ${cli} > /dev/null 2>&1); then
+	assert "failed to create Express-CLI symlink: ${cli_script}"; fi
+fi
 
 # Merge runtime arguments
 if [[ -n ${wizard_branch} ]]; then args+=" --branch ${wizard_branch}";fi
@@ -181,76 +243,12 @@ if [[ -n ${debug_flag} ]]; then
     fi
 fi
 
-# NEED TO SET THIS ENV VARIABLE SO IT WILL CREATE THE venv in the project
-#                 PIPENV_VENV_IN_PROJECT
-pip install --user pipenv
-mkdir -p ${wizard_basedir} 
-cd ${wizard_basedir}
-#pipenv install -e git+git://github.com/platform9/express-wizard.git@tomchris/setuptools#egg=express-wizard
-pipenv install -e git+git://github.com/platform9/express-wizard.git@${wizard_branch}
-# Enter a subshell in the virtualenv
-pipenv run wizard
-exit(0)
-eval pipenv shell
-wizard_venv=$(eval "pipenv --venv")
-debugging(${wizard_venv})
-launch_wizard="(. ${venv_activate} && ${venv_python} ${wizard_script}${args})"
-
-#pipenv run  # Run a command in the virtualenv
-
-
-########################################
-# REQUIRED PIP INSTALLS BEFORE INSTALLING WIZARD
-#
-#    python -m pip install --upgrade pip wheel setuptool
-#   
-#    setuptools-45.1.0
-#    
-
-
-
-
-
-exit(0)
-# configure python virtual environment
-debugging "Configuring virtualenv"
-if [ "$(ls -A ${wizard_venv} > /dev/null 2>&1; echo $?)" -ne 0 ]; then
-    for ver in {3,2}; do #ensure python3 is first
-	debugging "Checking Python${ver}: $(which python${ver})"
-        if [ "$(which python${ver} > /dev/null 2>&1)" ]; then
-	    python_version="$(python${ver} <<< 'import sys; print(sys.version_info[0])')"
-	    debugging "Python Version Selected: ${python_version}"
-	    break
-        #else
-	python_version=$(python -V) #Grep or split version from here and validate it is [2,3]
-        fi
-    done
-    init_venv_python
-else
-    echo "INFO: using exising virtual environment"
-fi
-
-launch_wizard="(. ${venv_activate} && ${venv_python} ${wizard_script}${args})"
-debugging "Wizard launch command: ${launch_wizard}"
-
-# start pf9-wizard in virtual environment
-flag_started=0
-while [ ${flag_started} -eq 0 ]; do
-    eval ${launch_wizard}
-    if [ $? -eq 0 ]; then
-        flag_started=1
-    else
-	stdout="$(eval ${launch_wizard} 2>&1 | grep 'Failed to import python module:')"
-	module_regex=".*Failed to import python module.*"
-	if [[ "${stdout}" =~ ${module_regex} ]]; then
-	    module_name=$(echo "${stdout}" | cut -d : -f3 | awk -F ' ' '{print $1}' | sed -e "s/'//g")
-	    echo "--> attempting to installing missing module: [${module_name}]"
-	    eval "(. ${venv_activate}; pip install ${module_name} > /dev/null 2>&1)"
-    	    if [ $? -ne 0 ]; then
-		assert "failed to install missing module: ${module_name}"
-	    fi
-        else
-	    assert "${stdout}"
-        fi
+if [[ -d "${pf9_bin}" ]]; then
+    if ! echo "$PATH" | grep -q "${pf9_bin}"; then
+        export PATH="${pf9_bin}:$PATH"
     fi
-done
+fi
+debugging "Wizard Launch Command: ${wizard}${args}"
+launch_wizard="(${venv_python} ${wizard}${args} -t)"
+
+#launch_wizard="(. ${venv_activate} && ${venv_python} ${wizard_script}${args})"
